bce50294315e303263851511c823c706d807c22f
Hancie123 <hanciewanemphago@gmail.com>
2024-10-15 21:31:50 +0545

    chatbot added

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -24,3 +24,21 @@
     <title>Nexo Server</title>
     <link rel="stylesheet" href="assets/css/main.css">
     {{-- <link rel="stylesheet" href="assets/css/custom.css"> --}}
+
+    <script>
+        (function(d,t) {
+          var BASE_URL="https://app.chatwoot.com";
+          var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+          g.src=BASE_URL+"/packs/js/sdk.js";
+          g.defer = true;
+          g.async = true;
+          s.parentNode.insertBefore(g,s);
+          g.onload=function(){
+            window.chatwootSDK.run({
+              websiteToken: 'jSw9TDsryzLn7GimizTNMo1i',
+              baseUrl: BASE_URL
+            })
+          }
+        })(document,"script");
+      </script>
+      
