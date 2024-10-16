2cffc44c67b00b141b44764c7c3992175eca09a7
Hancie123 <hanciewanemphago@gmail.com>
2024-10-16 20:09:50 +0545

    ui updated

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -28,6 +28,20 @@
 <link rel="stylesheet" href="assets/css/main.css">
 {{-- <link rel="stylesheet" href="assets/css/custom.css"> --}}
 
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RFFV78BPQY"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-RFFV78BPQY');
+</script>
+
 <script>
     window.chatwootSettings = {
         "position": "right",
