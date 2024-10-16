d25437c8830eb99f0fd6476ea0f9abfc9f4812e3
Hancie123 <hanciewanemphago@gmail.com>
2024-10-16 19:57:04 +0545

    ui updated

--- resources/views/front/whois/index.blade.php
+++ resources/views/front/whois/index.blade.php
@@ -12,11 +12,14 @@
 
     <div class="main">
 
-       
+
 
         <!--hero section start-->
-        <section class="hero-equal-height ptb-100"
-            style="background: url('assets/img/offer-bg-3.png')no-repeat center center / cover">
+        <section
+            class="hero-equal-height section-xl section-header background-video-overly text-white flex-column d-flex justify-content-center">
+            <video class="fit-cover w-100 h-100 position-absolute z--1" autoplay muted loop id="myVideo">
+                <source src="https://hostlar.themetags.com/server-room.mp4" type="video/mp4">
+            </video>
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-12 col-lg-8">
@@ -33,7 +36,8 @@
                                             placeholder="example.com" />
 
                                         <div class="input-group-append">
-                                            <button class="btn search-btn btn-hover d-flex align-items-center" type="submit" id="searchBtn">
+                                            <button class="btn search-btn btn-hover d-flex align-items-center"
+                                                type="submit" id="searchBtn">
                                                 <span class="ti-search mr-2"></span> <span id="buttonText">Search</span>
                                             </button>
                                         </div>
@@ -713,269 +717,6 @@
         </section>
         <!--start and transfer section end-->
 
-        <!--compare provider table start-->
-        <section class="compare-provider-table ptb-100 ">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-9 col-lg-8">
-                        <div class="section-heading text-center mb-5">
-                            <h2>HostLar head to head comparison</h2>
-                            <p class="lead">
-                                Globally incubate next-generation e-services via state of the art technology.
-                                Appropriately
-                                iterate quality.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12 col-lg-12">
-                        <div class="table-responsive">
-                            <table
-                                class="table w-100 table-hover table-bordered table-striped comparision-table text-center">
-                                <thead class="comparision-table-head">
-                                    <tr>
-                                        <td class="td-blank"></td>
-                                        <th class="primary-bg text-white provider-logo">
-                                            <img alt="hostlar-logo" src="assets/img/logo-white.png"
-                                                class="img-fluid" />
-                                        </th>
-                                        <th class="provider-logo">
-                                            <img alt="hostlar-logo" src="assets/img/clients-logo-01.png"
-                                                class="img-fluid" />
-                                        </th>
-                                        <th class="provider-logo">
-                                            <img alt="hostlar-logo" src="assets/img/clients-logo-02.png"
-                                                class="img-fluid" />
-                                        </th>
-                                        <th class="provider-logo">
-                                            <img alt="hostlar-logo" src="assets/img/clients-logo-03.png"
-                                                class="img-fluid" />
-                                        </th>
-                                        <th class="provider-logo">
-                                            <img alt="hostlar-logo" src="assets/img/clients-logo-04.png"
-                                                class="img-fluid" />
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody class="comparision-table-body">
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">Starting From</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <strong>$2.95</strong>
-                                            <small>/month</small>
-                                        </td>
-                                        <td>
-                                            <strong>$5.99</strong>
-                                            <small>/month</small>
-                                        </td>
-                                        <td>
-                                            <strong>$6.99</strong>
-                                            <small>/month</small>
-                                        </td>
-                                        <td>
-                                            <strong>$9.99</strong>
-                                            <small>/month</small>
-                                        </td>
-                                        <td>
-                                            <strong>$7.99</strong>
-                                            <small>/month</small>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">SSD Only servers</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <span class="ti-check"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-check"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">RocketBooster</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <span class="ti-check"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">Let's Encrypt</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <span class="ti-check"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left"> Cloudflare CDN Caching</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <span class="ti-check"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">SpamExperts</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <span class="ti-check"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">Web Application Firewall</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <span class="ti-check"></span>
-                                        </td>
-                                        <td> Paid</td>
-                                        <td> Paid</td>
-                                        <td> Paid</td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">RocketBooster</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <span class="ti-check"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">Free Daily Backups</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <span class="ti-check"></span>
-                                        </td>
-                                        <td class="">
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td> Paid</td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">Instant chat response</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <span class="ti-check"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                        <td>
-                                            <span class="ti-close text-danger"></span>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p class="text-left">Multiple server location</p>
-                                        </td>
-                                        <td class="primary-bg text-white">
-                                            <strong>10</strong>
-                                        </td>
-                                        <td>2</td>
-                                        <td>3</td>
-                                        <td>2</td>
-                                        <td>2</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!--compare provider table end-->
 
         <!--testimonial and review section start-->
         {{-- @include('front.components.testimonials') --}}
@@ -1000,21 +741,21 @@
         }
     </style>
 
-<script>
-    function showLoader() {
-        const searchBtn = document.getElementById('searchBtn');
-        const buttonText = document.getElementById('buttonText');
-        
-        buttonText.innerHTML = 'Searching...';
-        searchBtn.disabled = true; 
-        
-        // Create loader element
-        const loader = document.createElement('div');
-        loader.classList.add('loader');
-        loader.style.marginLeft = '8px'; 
-        searchBtn.appendChild(loader);
-    }
-</script>
+    <script>
+        function showLoader() {
+            const searchBtn = document.getElementById('searchBtn');
+            const buttonText = document.getElementById('buttonText');
+
+            buttonText.innerHTML = 'Searching...';
+            searchBtn.disabled = true;
+
+            // Create loader element
+            const loader = document.createElement('div');
+            loader.classList.add('loader');
+            loader.style.marginLeft = '8px';
+            searchBtn.appendChild(loader);
+        }
+    </script>
 
 
 
