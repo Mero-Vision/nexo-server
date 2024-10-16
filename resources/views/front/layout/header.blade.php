358891adc3f8855054d1f05a4a20a5257e8370bb
Hancie123 <hanciewanemphago@gmail.com>
2024-10-16 20:00:30 +0545

    ui updated

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -1,44 +1,52 @@
 <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <!-- SEO Meta description -->
-    <meta name="description" content="Unlock your online potential with Nexo Server, your trusted hosting and domain provider. Enjoy lightning-fast hosting, robust security, and personalized support to ensure your website thrives. Get started today!">
-    <meta name="author" content="Mero Vision">
+<!-- SEO Meta description -->
+<meta name="description"
+    content="Unlock your online potential with Nexo Server, your trusted hosting and domain provider. Enjoy lightning-fast hosting, robust security, and personalized support to ensure your website thrives. Get started today!">
+<meta name="author" content="Mero Vision">
 
-    <meta property="og:site_name" content="" /> <!-- website name -->
-    <meta property="og:site" content="" /> <!-- website link -->
-    <meta property="og:title" content="" /> <!-- title shown in the actual shared post -->
-    <meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
-    <meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
-    <meta property="og:url" content="" /> <!-- where do you want your post to link to -->
-    <meta property="og:type" content="article" />
+<meta property="og:site_name" content="" /> <!-- website name -->
+<meta property="og:site" content="" /> <!-- website link -->
+<meta property="og:title" content="" /> <!-- title shown in the actual shared post -->
+<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
+<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
+<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
+<meta property="og:type" content="article" />
 
-    <!--favicon icon-->
-    <link rel="icon" href="{{url('assets/img/logo.png')}}" type="image/png" sizes="16x16">
+<!--favicon icon-->
+<link rel="icon" href="{{ url('assets/img/logo.png') }}" type="image/png" sizes="16x16">
 
-    <!--google fonts-->
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+<!--google fonts-->
+<link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Poppins:wght@400;500;600;700&display=swap"
+    rel="stylesheet">
 
-    <!--title-->
-    <title>Nexo Server</title>
-    <link rel="stylesheet" href="assets/css/main.css">
-    {{-- <link rel="stylesheet" href="assets/css/custom.css"> --}}
+<!--title-->
+<title>Nexo Server</title>
+<link rel="stylesheet" href="assets/css/main.css">
+{{-- <link rel="stylesheet" href="assets/css/custom.css"> --}}
 
-    <script>
-        (function(d,t) {
-          var BASE_URL="https://app.chatwoot.com";
-          var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-          g.src=BASE_URL+"/packs/js/sdk.js";
-          g.defer = true;
-          g.async = true;
-          s.parentNode.insertBefore(g,s);
-          g.onload=function(){
+<script>
+    window.chatwootSettings = {
+        "position": "right",
+        "type": "expanded_bubble",
+        "launcherTitle": "Chat with us"
+    };
+    (function(d, t) {
+        var BASE_URL = "https://app.chatwoot.com";
+        var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+        g.src = BASE_URL + "/packs/js/sdk.js";
+        g.defer = true;
+        g.async = true;
+        s.parentNode.insertBefore(g, s);
+        g.onload = function() {
             window.chatwootSDK.run({
-              websiteToken: 'jSw9TDsryzLn7GimizTNMo1i',
-              baseUrl: BASE_URL
+                websiteToken: 'jSw9TDsryzLn7GimizTNMo1i',
+                baseUrl: BASE_URL
             })
-          }
-        })(document,"script");
-      </script>
-      
+        }
+    })(document, "script");
+</script>
